b764a15aec33c41e4a67c867eea0b0fd25109d4f
aleksandrkuznecov <aikuznetsov@sberbank.ru>
2022-05-22 23:33:52 +0300

    добавлены новые разделы

--- src/main/java/core/exceptions/Test.java
+++ src/main/java/core/exceptions/Test.java
@@ -9,8 +9,21 @@ public class Test {
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
+
+        try {
+            throwError();
+        } catch (Error er) {
+            System.out.println(er.getMessage());
+        }
+
+//        throw new RuntimeException("Сорян");
     }
+
     static void throwException() {
-        throw new RuntimeException("пизда");
+        throw new MyException("Исключение");
+    }
+
+    static void throwError() {
+        throw new MyError("Ошибка");
     }
 }
